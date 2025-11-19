459bfe2351dc014cabfec1bbb13e859007b7a37e
Yeizermarrugo <yeizermarrugo@gmail.com>
2025-11-19 11:39:20 -0500

    Agregar vista de tarjeta de información con detalles de la herramienta

---
+++ resources/views/infoCard.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('title', $info['nombre'] . ' | Información de herramienta')
+
+@section('content')
+    <div class="info-card">
+        <h2>{{ $info['nombre'] }}</h2>
+        <p><strong>Descripción:</strong> {{ $info['descripcion'] }}</p>
+        <p><strong>Categoría:</strong> {{ $info['categoria'] }}</p>
+        <p><strong>Limitaciones:</strong> {{ $info['limitaciones'] }}</p>
+        <p><strong>Requisitos:</strong> {{ $info['insumos'] }}</p>
+        <p><strong>Pasos:</strong> {{ $info['pasos'] }}</p>
+        <p><strong>Resultado:</strong> {{ $info['resultado'] }}</p>
+        <p>
+            <a href="{{ $info['enlace'] }}" target="_blank" class="btn btn-primary">Ir a la herramienta</a>
+            @if ($info['video'])
+                <a href="{{ $info['video'] }}" target="_blank" class="btn btn-secondary">Video tutorial</a>
+            @endif
+        </p>
+    </div>
+@endsection
