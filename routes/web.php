83622e34ed24911c77079a69e55f73d3acd271fb
Yeizermarrugo <yeizermarrugo@gmail.com>
2025-11-19 11:39:45 -0500

    Modificar rutas: actualizar la vista de inicio y reorganizar la ruta de información

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\InfoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/info/{nombre}', [InfoController::class, 'show'])->name('info.show');
 
-Route::get('/repositorio', function () {
+Route::get('/', function () {
     return view('repositorio');
 });
