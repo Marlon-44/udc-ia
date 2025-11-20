499b4e406718e9b47fa4c07835d0127c0a0afd7e
Yeizermarrugo <yeizermarrugo@gmail.com>
2025-11-19 23:41:21 -0500

    agregar ruta para mostrar información detallada de herramientas

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\InfoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/info/{nombre}', [InfoController::class, 'show'])->name('info.show');
 
 Route::get('/repositorio', function () {
     return view('repositorio');
