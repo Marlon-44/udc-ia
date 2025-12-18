975abea76f4dca51ed04024cc41fc17bd59f7a65
Yeizermarrugo <yeizermarrugo@gmail.com>
2025-12-18 10:07:41 -0500

    mejorar el diseño de la sección de PDFs: actualizar estilos CSS y modificar la estructura HTML para una mejor presentación

--- resources/js/renderPdfsPage.js
+++ resources/js/renderPdfsPage.js
@@ -3,17 +3,19 @@ const index = params.get("index");
 const container = document.getElementById("pdfs-container");
 
 fetch(JSON_URL)
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
         const carpeta = data[index];
 
-        carpeta.pdfs.forEach(pdf => {
-            container.insertAdjacentHTML("beforeend", `
-                <div class="pdf-item">
-                    <a target="_blank" href="${pdf}">
-                        Ver PDF
-                    </a>
-                </div>
-            `);
+        carpeta.pdfs.forEach((pdf) => {
+            container.insertAdjacentHTML(
+                "beforeend",
+                `
+        <a class="pdf-card" target="_blank" href="${pdf}">
+          <span class="pdf-card__title">${pdf.split("/").pop()}</span>
+          <img class="pdf-card__icon" src="images/download.png" alt="Descargar PDF">
+        </a>
+      `
+            );
         });
     });
